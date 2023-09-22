cb25a0d5cc0cbdc7d7d12ee4af219c86d328608b
Diogo Dias <74829049+PsyRullez@users.noreply.github.com>
2023-09-22 17:41:23 +0100

    Add files via upload
    TicTacToe - GAME

---
+++ Prog22DiogoDias/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Prog22DiogoDias
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btn1 = New System.Windows.Forms.Button()
+        Me.Btn2 = New System.Windows.Forms.Button()
+        Me.Btn3 = New System.Windows.Forms.Button()
+        Me.Btn4 = New System.Windows.Forms.Button()
+        Me.Btn5 = New System.Windows.Forms.Button()
+        Me.Btn6 = New System.Windows.Forms.Button()
+        Me.Btn7 = New System.Windows.Forms.Button()
+        Me.Btn8 = New System.Windows.Forms.Button()
+        Me.Btn9 = New System.Windows.Forms.Button()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.BtnClose = New System.Windows.Forms.Button()
+        Me.LblDev = New System.Windows.Forms.Label()
+        Me.LblPlayer = New System.Windows.Forms.Label()
+        Me.LblTittle = New System.Windows.Forms.Label()
+        Me.BtnPlayerList = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Btn1
+        '
+        Me.Btn1.BackColor = System.Drawing.Color.White
+        Me.Btn1.Location = New System.Drawing.Point(131, 110)
+        Me.Btn1.Name = "Btn1"
+        Me.Btn1.Size = New System.Drawing.Size(58, 42)
+        Me.Btn1.TabIndex = 0
+        Me.Btn1.UseVisualStyleBackColor = False
+        '
+        'Btn2
+        '
+        Me.Btn2.BackColor = System.Drawing.Color.White
+        Me.Btn2.Location = New System.Drawing.Point(131, 168)
+        Me.Btn2.Name = "Btn2"
+        Me.Btn2.Size = New System.Drawing.Size(58, 42)
+        Me.Btn2.TabIndex = 1
+        Me.Btn2.UseVisualStyleBackColor = False
+        '
+        'Btn3
+        '
+        Me.Btn3.BackColor = System.Drawing.Color.White
+        Me.Btn3.Location = New System.Drawing.Point(131, 231)
+        Me.Btn3.Name = "Btn3"
+        Me.Btn3.Size = New System.Drawing.Size(58, 42)
+        Me.Btn3.TabIndex = 2
+        Me.Btn3.UseVisualStyleBackColor = False
+        '
+        'Btn4
+        '
+        Me.Btn4.BackColor = System.Drawing.Color.White
+        Me.Btn4.Location = New System.Drawing.Point(217, 110)
+        Me.Btn4.Name = "Btn4"
+        Me.Btn4.Size = New System.Drawing.Size(58, 42)
+        Me.Btn4.TabIndex = 3
+        Me.Btn4.UseVisualStyleBackColor = False
+        '
+        'Btn5
+        '
+        Me.Btn5.BackColor = System.Drawing.Color.White
+        Me.Btn5.Location = New System.Drawing.Point(217, 168)
+        Me.Btn5.Name = "Btn5"
+        Me.Btn5.Size = New System.Drawing.Size(58, 42)
+        Me.Btn5.TabIndex = 4
+        Me.Btn5.UseVisualStyleBackColor = False
+        '
+        'Btn6
+        '
+        Me.Btn6.BackColor = System.Drawing.Color.White
+        Me.Btn6.Location = New System.Drawing.Point(217, 231)
+        Me.Btn6.Name = "Btn6"
+        Me.Btn6.Size = New System.Drawing.Size(58, 42)
+        Me.Btn6.TabIndex = 5
+        Me.Btn6.UseVisualStyleBackColor = False
+        '
+        'Btn7
+        '
+        Me.Btn7.BackColor = System.Drawing.Color.White
+        Me.Btn7.Location = New System.Drawing.Point(304, 110)
+        Me.Btn7.Name = "Btn7"
+        Me.Btn7.Size = New System.Drawing.Size(58, 42)
+        Me.Btn7.TabIndex = 6
+        Me.Btn7.UseVisualStyleBackColor = False
+        '
+        'Btn8
+        '
+        Me.Btn8.BackColor = System.Drawing.Color.White
+        Me.Btn8.Location = New System.Drawing.Point(304, 168)
+        Me.Btn8.Name = "Btn8"
+        Me.Btn8.Size = New System.Drawing.Size(58, 42)
+        Me.Btn8.TabIndex = 7
+        Me.Btn8.UseVisualStyleBackColor = False
+        '
+        'Btn9
+        '
+        Me.Btn9.BackColor = System.Drawing.Color.White
+        Me.Btn9.Location = New System.Drawing.Point(304, 228)
+        Me.Btn9.Name = "Btn9"
+        Me.Btn9.Size = New System.Drawing.Size(58, 42)
+        Me.Btn9.TabIndex = 8
+        Me.Btn9.UseVisualStyleBackColor = False
+        '
+        'BtnStart
+        '
+        Me.BtnStart.BackColor = System.Drawing.Color.Silver
+        Me.BtnStart.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.BtnStart.Location = New System.Drawing.Point(191, 378)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(130, 47)
+        Me.BtnStart.TabIndex = 9
+        Me.BtnStart.Text = "Iniciar Jogo"
+        Me.BtnStart.UseVisualStyleBackColor = False
+        '
+        'BtnClose
+        '
+        Me.BtnClose.BackColor = System.Drawing.Color.Red
+        Me.BtnClose.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.BtnClose.Location = New System.Drawing.Point(391, 378)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(108, 47)
+        Me.BtnClose.TabIndex = 10
+        Me.BtnClose.Text = "Sair"
+        Me.BtnClose.UseVisualStyleBackColor = False
+        '
+        'LblDev
+        '
+        Me.LblDev.AutoSize = True
+        Me.LblDev.Location = New System.Drawing.Point(446, 455)
+        Me.LblDev.Name = "LblDev"
+        Me.LblDev.Size = New System.Drawing.Size(56, 13)
+        Me.LblDev.TabIndex = 11
+        Me.LblDev.Text = "DiogoDias"
+        '
+        'LblPlayer
+        '
+        Me.LblPlayer.BackColor = System.Drawing.Color.Silver
+        Me.LblPlayer.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblPlayer.Location = New System.Drawing.Point(132, 302)
+        Me.LblPlayer.Name = "LblPlayer"
+        Me.LblPlayer.Size = New System.Drawing.Size(230, 37)
+        Me.LblPlayer.TabIndex = 12
+        Me.LblPlayer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblTittle
+        '
+        Me.LblTittle.BackColor = System.Drawing.Color.Olive
+        Me.LblTittle.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblTittle.Location = New System.Drawing.Point(128, 27)
+        Me.LblTittle.Name = "LblTittle"
+        Me.LblTittle.Size = New System.Drawing.Size(230, 37)
+        Me.LblTittle.TabIndex = 13
+        Me.LblTittle.Text = "Jogo do Galo"
+        Me.LblTittle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'BtnPlayerList
+        '
+        Me.BtnPlayerList.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.BtnPlayerList.Location = New System.Drawing.Point(12, 378)
+        Me.BtnPlayerList.Name = "BtnPlayerList"
+        Me.BtnPlayerList.Size = New System.Drawing.Size(130, 47)
+        Me.BtnPlayerList.TabIndex = 14
+        Me.BtnPlayerList.Text = "Ranking"
+        Me.BtnPlayerList.UseVisualStyleBackColor = True
+        '
+        'Prog22DiogoDias
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Moccasin
+        Me.ClientSize = New System.Drawing.Size(523, 477)
+        Me.Controls.Add(Me.BtnPlayerList)
+        Me.Controls.Add(Me.LblTittle)
+        Me.Controls.Add(Me.LblPlayer)
+        Me.Controls.Add(Me.LblDev)
+        Me.Controls.Add(Me.BtnClose)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Controls.Add(Me.Btn9)
+        Me.Controls.Add(Me.Btn8)
+        Me.Controls.Add(Me.Btn7)
+        Me.Controls.Add(Me.Btn6)
+        Me.Controls.Add(Me.Btn5)
+        Me.Controls.Add(Me.Btn4)
+        Me.Controls.Add(Me.Btn3)
+        Me.Controls.Add(Me.Btn2)
+        Me.Controls.Add(Me.Btn1)
+        Me.Name = "Prog22DiogoDias"
+        Me.Text = "Prog22DiogoDias"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btn1 As Button
+    Friend WithEvents Btn2 As Button
+    Friend WithEvents Btn3 As Button
+    Friend WithEvents Btn4 As Button
+    Friend WithEvents Btn5 As Button
+    Friend WithEvents Btn6 As Button
+    Friend WithEvents Btn7 As Button
+    Friend WithEvents Btn8 As Button
+    Friend WithEvents Btn9 As Button
+    Friend WithEvents BtnStart As Button
+    Friend WithEvents BtnClose As Button
+    Friend WithEvents LblDev As Label
+    Friend WithEvents LblPlayer As Label
+    Friend WithEvents LblTittle As Label
+    Friend WithEvents BtnPlayerList As Button
+End Class
---
+++ Prog22DiogoDias/Form1.vb
@@ -0,0 +1,295 @@
+﻿Imports System.Data.OleDb
+
+
+Public Class Prog22DiogoDias
+
+    Dim myconnect As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\TicTacToe_db.accdb")
+
+    Private jogadoresDefinidos As Boolean = False
+    Private jogador1 As String
+    Private jogador2 As String
+    Private pontuacaoJogador1 As Integer = 0
+    Private pontuacaoJogador2 As Integer = 0
+    Private turnoJogador1 As Boolean = True ' Começa com o jogador 1
+    Private Jogo(10) As Integer
+    Private somas(8) As Integer
+
+    Private Function IsUsernameExists(text As Object) As Boolean
+        Throw New NotImplementedException()
+    End Function
+
+    Private Sub Verificar()
+        ' Verificar se algum jogador venceu
+        ' Horizontal
+        somas(1) = Jogo(1) + Jogo(2) + Jogo(3)
+        somas(2) = Jogo(4) + Jogo(5) + Jogo(6)
+        somas(3) = Jogo(7) + Jogo(8) + Jogo(9)
+        ' Vertical
+        somas(4) = Jogo(1) + Jogo(4) + Jogo(7)
+        somas(5) = Jogo(2) + Jogo(5) + Jogo(8)
+        somas(6) = Jogo(3) + Jogo(6) + Jogo(9)
+        ' Obliquas
+        somas(7) = Jogo(1) + Jogo(5) + Jogo(9)
+        somas(8) = Jogo(3) + Jogo(5) + Jogo(7)
+
+        For i = 1 To 8
+            If somas(i) = 3 Then
+                LblPlayer.Text = "Ganhou o jogador: " & jogador1
+                terminar()
+            End If
+            If somas(i) = 30 Then
+                LblPlayer.Text = "Ganhou o jogador: " & jogador2
+                terminar()
+            End If
+
+        Next
+
+
+
+
+        If pontuacaoJogador1 = 3 Then
+            LblPlayer.Text = jogador1 & " venceu!"
+        ElseIf pontuacaoJogador2 = 30 Then
+            LblPlayer.Text = jogador2 & " venceu!"
+        End If
+    End Sub
+
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        Close()
+    End Sub
+
+    Private Sub terminar()
+        Btn1.Enabled = False
+        Btn2.Enabled = False
+        Btn3.Enabled = False
+        Btn4.Enabled = False
+        Btn5.Enabled = False
+        Btn6.Enabled = False
+        Btn7.Enabled = False
+        Btn8.Enabled = False
+        Btn9.Enabled = False
+    End Sub
+
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        'Botoes TicTacToe ON
+        Btn1.Enabled = True
+        Btn2.Enabled = True
+        Btn3.Enabled = True
+        Btn4.Enabled = True
+        Btn5.Enabled = True
+        Btn6.Enabled = True
+        Btn7.Enabled = True
+        Btn8.Enabled = True
+        Btn9.Enabled = True
+
+        If Not jogadoresDefinidos Then
+            ' Solicitar o nome do Jogador 1
+            jogador1 = InputBox("Digite o nome do Jogador 1:", "Nome do Jogador 1")
+
+            ' Solicitar o nome do Jogador 2
+            jogador2 = InputBox("Digite o nome do Jogador 2:", "Nome do Jogador 2")
+
+            ' Verificar se os nomes foram inseridos e exibir uma mensagem
+            If Not String.IsNullOrWhiteSpace(jogador1) AndAlso Not String.IsNullOrWhiteSpace(jogador2) Then
+                MessageBox.Show("Nomes dos jogadores foram definidos:" & vbCrLf &
+                            "Jogador 1: " & jogador1 & vbCrLf &
+                            "Jogador 2: " & jogador2, "Nomes dos Jogadores")
+
+                ' Definir a flag jogadoresDefinidos como True para não pedir nomes novamente
+                jogadoresDefinidos = True
+            Else
+                MessageBox.Show("Por favor, insira nomes válidos para ambos os jogadores.", "Erro")
+            End If
+        End If
+    End Sub
+
+    Private Sub Prog22DiogoDias_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TicTacToe Botoes OFF
+        Btn1.Enabled = False
+        Btn2.Enabled = False
+        Btn3.Enabled = False
+        Btn4.Enabled = False
+        Btn5.Enabled = False
+        Btn6.Enabled = False
+        Btn7.Enabled = False
+        Btn8.Enabled = False
+        Btn9.Enabled = False
+    End Sub
+
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        If Btn1.Text = "" Then ' Verifica se o botão está vazio
+            If turnoJogador1 Then
+                Btn1.Text = "O"
+                Jogo(1) = 1
+            Else
+                Btn1.Text = "X"
+                Jogo(1) = 10
+            End If
+
+            ' Alternar o turno
+            turnoJogador1 = Not turnoJogador1
+
+            ' Desativar o botão após ser clicado
+            Btn1.Enabled = False
+
+            Verificar()
+            LblPlayer.Focus()
+        End If
+    End Sub
+
+    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
+        If Btn2.Text = "" Then ' Verifica se o botão está vazio
+            If turnoJogador1 Then
+                Btn2.Text = "O"
+                Jogo(2) = 1
+            Else
+                Btn2.Text = "X"
+                Jogo(2) = 10
+            End If
+
+            ' Alternar o turno
+            turnoJogador1 = Not turnoJogador1
+
+            ' Desativar o botão após ser clicado
+            Btn2.Enabled = False
+            Verificar()
+        End If
+
+    End Sub
+
+    Private Sub Btn3_Click(sender As Object, e As EventArgs) Handles Btn3.Click
+        If Btn3.Text = "" Then ' Verifica se o botão está vazio
+            If turnoJogador1 Then
+                Btn3.Text = "O"
+                Jogo(3) = 1
+            Else
+                Btn3.Text = "X"
+                Jogo(3) = 10
+            End If
+
+            ' Alternar o turno
+            turnoJogador1 = Not turnoJogador1
+
+            ' Desativar o botão após ser clicado
+            Btn3.Enabled = False
+            Verificar()
+        End If
+    End Sub
+
+    Private Sub Btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
+        If Btn4.Text = "" Then ' Verifica se o botão está vazio
+            If turnoJogador1 Then
+                Btn4.Text = "O"
+                Jogo(4) = 1
+            Else
+                Btn4.Text = "X"
+                Jogo(4) = 10
+            End If
+
+            ' Alternar o turno
+            turnoJogador1 = Not turnoJogador1
+
+            ' Desativar o botão após ser clicado
+            Btn4.Enabled = False
+            Verificar()
+        End If
+    End Sub
+
+    Private Sub Btn5_Click(sender As Object, e As EventArgs) Handles Btn5.Click
+        If Btn5.Text = "" Then ' Verifica se o botão está vazio
+            If turnoJogador1 Then
+                Btn5.Text = "O"
+                Jogo(5) = 1
+            Else
+                Btn5.Text = "X"
+                Jogo(5) = 10
+            End If
+
+            ' Alternar o turno
+            turnoJogador1 = Not turnoJogador1
+
+            ' Desativar o botão após ser clicado
+            Btn5.Enabled = False
+            Verificar()
+        End If
+    End Sub
+
+    Private Sub Btn6_Click(sender As Object, e As EventArgs) Handles Btn6.Click
+        If Btn6.Text = "" Then ' Verifica se o botão está vazio
+            If turnoJogador1 Then
+                Btn6.Text = "O"
+                Jogo(6) = 1
+            Else
+                Btn6.Text = "X"
+                Jogo(6) = 10
+            End If
+
+            ' Alternar o turno
+            turnoJogador1 = Not turnoJogador1
+
+            ' Desativar o botão após ser clicado
+            Btn6.Enabled = False
+            Verificar()
+        End If
+    End Sub
+
+    Private Sub Btn7_Click(sender As Object, e As EventArgs) Handles Btn7.Click
+        If Btn7.Text = "" Then ' Verifica se o botão está vazio
+            If turnoJogador1 Then
+                Btn7.Text = "O"
+                Jogo(7) = 1
+            Else
+                Btn7.Text = "X"
+                Jogo(7) = 10
+            End If
+
+            ' Alternar o turno
+            turnoJogador1 = Not turnoJogador1
+
+            ' Desativar o botão após ser clicado
+            Btn7.Enabled = False
+            Verificar()
+        End If
+    End Sub
+
+    Private Sub Btn8_Click(sender As Object, e As EventArgs) Handles Btn8.Click
+        If Btn8.Text = "" Then ' Verifica se o botão está vazio
+            If turnoJogador1 Then
+                Btn8.Text = "O"
+                Jogo(8) = 1
+            Else
+                Btn8.Text = "X"
+                Jogo(8) = 10
+            End If
+
+            ' Alternar o turno
+            turnoJogador1 = Not turnoJogador1
+
+            ' Desativar o botão após ser clicado
+            Btn8.Enabled = False
+            Verificar()
+        End If
+    End Sub
+
+    Private Sub Btn9_Click(sender As Object, e As EventArgs) Handles Btn9.Click
+        If Btn9.Text = "" Then ' Verifica se o botão está vazio
+            If turnoJogador1 Then
+                Btn9.Text = "O"
+                Jogo(9) = 1
+            Else
+                Btn9.Text = "X"
+                Jogo(9) = 10
+            End If
+
+            ' Alternar o turno
+            turnoJogador1 = Not turnoJogador1
+
+            ' Desativar o botão após ser clicado
+            Btn9.Enabled = False
+            Verificar()
+        End If
+
+
+    End Sub
+End Class
